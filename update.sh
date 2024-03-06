64d54c6dbe77bc92180b5a04a0cca37d6ec54dcd
skshetry <18718008+skshetry@users.noreply.github.com>
2024-03-06 09:18:03 +0545

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,11 +5,11 @@ set -x
 
 LATEST=$1
 if [ ! $LATEST ]; then
-  LATEST="3.48.4"
+  LATEST="3.48.5"
 fi
 
 PROJECT="iterative/dvc"
 GHAPI_URL="https://api.github.com/repos/$PROJECT/releases/latest"
-LATEST="3.48.4"
+LATEST="3.48.5"
 
 sed -i 's/^VERSION = .*$/VERSION = '"\"$LATEST\""'/g' download.py
